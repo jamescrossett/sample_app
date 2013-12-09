d099a147d66a7fc3fc96776152cc8b1a143e8d7d
James Crossett <jcrossett@me.com>
2013-12-09 22:09:04 +0000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "James Crossett"
+    email    "jcrossett@me.com"
+    password "Testing1984"
+    password_confirmation "Testing1984"
+  end
+end
